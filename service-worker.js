df7b2fa2d4a1862997d7d0cdb3f575a15f9e5c78
Sai Tharun Aditya <saitarunaditya@gmail.com>
2025-04-21 19:53:56 +0530

    modified sw.js, icons added and new manifest.json

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,35 @@
+const CACHE_NAME = 'object-gemini-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/static/icons/icon-192.png',
+  '/static/icons/icon-512.png'
+];
+
 self.addEventListener('install', (event) => {
   console.log('Service Worker installing.');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   console.log('Service Worker activating.');
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      )
+    )
+  );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
